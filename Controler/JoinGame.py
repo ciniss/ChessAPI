c2b73a8a661c571676c2a5f83fb540fd71d6798f
Bartłomiej Dąbrowski <73825209+ciniss@users.noreply.github.com>
2022-01-23 14:28:06 +0100

    someasojdhna

--- Controler/JoinGame.py
+++ Controler/JoinGame.py
@@ -2,24 +2,36 @@ from Models.Game import Game
 from Models.User import User
 import uuid
 from base import Session, Base, engine
+from random import randint
 
 
-def join_chess_game(gid, uid: uuid.UUID):
+def join_chess_game(gid: int, uid: uuid.UUID):
     Base.metadata.create_all(engine)
     session = Session()
     game = session.query(Game).where(Game.id == gid).first()
     if game is not None:
-        if game.white_player is None:
-            session.query(Game).filter(Game.id == gid).update({"white_player": uid})
-            session.commit()
-            session.close()
-            return gid
-        elif game.black_player is None:
-            #game.black_player = uid
-            session.query(Game).filter(Game.id == gid).update( {"black_player": uid})
-            session.commit()
-            session.close()
-            return gid
+        if game.white_player is None and game.black_player is None:
+            p = randint(4, 22) % 2
+            if p == 0:
+                session.query(Game).filter(Game.id == gid).update({"white_player": uid})
+                session.commit()
+                session.close()
+            else:
+                session.query(Game).filter(Game.id == gid).update({"black_player": uid})
+                session.commit()
+                session.close()
+        else:
+            if game.white_player is None and game.black_player != uid:
+                session.query(Game).filter(Game.id == gid).update({"white_player": uid})
+                session.commit()
+                session.close()
+            elif game.black_player is None and game.white_player != uid:
+                session.query(Game).filter(Game.id == gid).update({"black_player": uid})
+                session.commit()
+                session.close()
+            else:
+                return -1
+        return gid
     session.close()
     return -1
 
